1cac7a5ad22103880d91c1d87ba044963472a33c
junaaid96 <junaidul.islam@egeneration.co>
2024-11-18 18:21:53 +0600

    name refactored

---
+++ SpringBoot/src/main/java/org/example/springboot/HomeController.java
@@ -0,0 +1,28 @@
+package org.example.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/add")
+//    or, public String add(int num1, int num2, Model model){}
+//    or, public String add(HttpServletRequest req, HttpSession session){session.setAttribute()}
+    public ModelAndView add(@RequestParam("num1") int num, @RequestParam("num2") int num2, ModelAndView mv) {
+
+        int result = num + num2;
+
+        mv.addObject("result", result);
+        mv.setViewName("result");
+
+        return mv;
+    }
+}
