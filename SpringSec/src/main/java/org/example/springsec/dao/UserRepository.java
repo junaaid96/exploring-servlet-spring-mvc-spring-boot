e7451a1ac9802fadc11e49e3703b7fd61b062208
junaaid96 <junaidul.islam@egeneration.co>
2024-11-26 18:09:54 +0600

    ...

---
+++ SpringSec/src/main/java/org/example/springsec/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.springsec.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository implements JpaRepository {
+
+}
